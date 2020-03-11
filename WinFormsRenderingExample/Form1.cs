23d929af26876120021ae6e72f9d7b14fd33df46
Zak Fenton <zak.fenton@outlook.com>
2020-03-11 21:17:24 +1100

    Basic changes to the usual "Windows Forms Application" template to add custom form with a simple chessboard-like pattern.

--- WinFormsRenderingExample/Form1.cs
+++ WinFormsRenderingExample/Form1.cs
@@ -15,6 +15,13 @@ namespace WinFormsRenderingExample
         public Form1()
         {
             InitializeComponent();
+
+            // In a more advanced app you might want to add handles for the form designer to pick this class up.
+            // For this, I've just added the control manually:
+            RendererControl rc = new RendererControl();
+            rc.Location = new Point(0, 0);
+            rc.MinimumSize = new Size(300, 300);
+            Controls.Add(rc);
         }
     }
 }
--- WinFormsRenderingExample/Program.cs
+++ WinFormsRenderingExample/Program.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormsRenderingExample
 {
+    public class RendererControl : Control
+    {
+
+
+        public Bitmap CreateBitmap(int w, int h)
+        {
+            Bitmap result = new Bitmap(w, h);
+            Graphics graphics = Graphics.FromImage(result);
+            int i = 0;
+            for (int y = 0; y < h; y += 64)
+            {
+                for (int x = 0; x < w; x += 64)
+                {
+                    if (i % 2 != 0)
+                    {
+                        graphics.FillRectangle(Brushes.Black, x, y, 64, 64);
+                    } else
+                    {
+                        graphics.FillRectangle(Brushes.White, x, y, 64, 64);
+                    }
+                    i++;
+                }
+            }
+            return result;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawImage(CreateBitmap(300, 300),0,0);
+        }
+    }
+
     static class Program
     {
         /// <summary>
